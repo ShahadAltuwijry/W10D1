92099d149c61e8c330bab695ff70ba3d6db44c3a
ShahadAltuwijry <Shahad.abdallah.elq.js@tuwaiq.edu.sa>
2021-12-12 14:57:49 +0300

    pages title

--- pages/Posts/index.js
+++ pages/Posts/index.js
@@ -5,8 +5,6 @@ export async function getStaticProps() {
   const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
   const posts = await res.json();
 
-console.log(posts);
-
   if (!posts) {
     return {
       notFound: true,
