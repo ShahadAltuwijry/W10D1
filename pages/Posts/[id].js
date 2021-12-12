2715cda72a2e5dd771edf893e80d491252a51252
ShahadAltuwijry <Shahad.abdallah.elq.js@tuwaiq.edu.sa>
2021-12-12 16:13:03 +0300

    confirm page basic css

--- pages/Posts/[id].js
+++ pages/Posts/[id].js
@@ -15,11 +15,12 @@ export async function getStaticPaths() {
 }
 
 // This also gets called at build time
-export async function getStaticProps({ context }) {
-  const id = context.params.id;
+export async function getStaticProps({ params }) {
   // params contains the post `id`.
   // If the route is like /posts/1, then params.id is 1
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${params.id}`
+  );
   const post = await res.json();
 
   // Pass post data to the page via props
@@ -29,9 +30,9 @@ export async function getStaticProps({ context }) {
 function Post({ post }) {
   return (
     <>
-      <div>
+      <div style={{ margin: "15px" }}>
         <h1>post Details</h1>
-        <div key={post.id}>
+        <div key={post.id} style={{ borderBottom: "1px solid black" }}>
           <h2> - {post.title}</h2>
           <p>{post.body}</p>
         </div>
